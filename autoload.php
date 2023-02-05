5397d7e605d2553b38cff62bbb334ec07310c11e
Jowi <joaobalaniuc>
2023-02-05 12:42:44 -0300

    beta

---
+++ autoload.php
@@ -0,0 +1,3 @@
+<?php
+require(__DIR__ . "/class/ApiServer.php");
+require(__DIR__ . "/class/ApiServer.http.php");
---
+++ mason/mason.api-server.php
@@ -0,0 +1,8 @@
+<?php
+class apiserver extends Mason
+{
+    public function __construct()
+    {
+        $this->say("works!!", false, "green");
+    }
+}
